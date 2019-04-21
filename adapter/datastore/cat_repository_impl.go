502211560eb865597ace956614ef061c27bf1d0f
Yu Ishiguro <yuuis.egr@gmail.com>
2019-04-21 22:47:41 +0900

    add adapters

--- adapter/datastore/cat_repository_impl.go
+++ adapter/datastore/cat_repository_impl.go
@@ -1,1 +1,56 @@
 package datastore
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/yuuis/cat-api-go/adapter/datastore/mysql"
+	"github.com/yuuis/cat-api-go/domain/entity"
+	"github.com/yuuis/cat-api-go/domain/repository"
+)
+
+type cat struct {
+	db *gorm.DB
+}
+
+func NewCat(db *gorm.DB) repository.Cat {
+	return &cat{
+		db: db,
+	}
+}
+
+func (c *cat) All() ([]*entity.Cat, error) {
+	var mCats []mysql.Cat
+
+	if err := c.db.Find(&mCats).Error; err != nil {
+		return nil, err
+	}
+
+	var eCats []*entity.Cat
+	for _, mCat := range mCats {
+		eCats = append(eCats, mCat.ToEntity())
+	}
+
+	return eCats, nil
+}
+
+func (c *cat) Find(id int64) (*entity.Cat, error) {
+	var mCat mysql.Cat
+
+	if err := c.db.First(&mCat, id).Error; err != nil {
+		return nil, err
+	}
+
+	return mCat.ToEntity(), nil
+}
+
+func (c *cat) Store(cat *entity.Cat) (*entity.Cat, error) {
+	mCat := mysql.Cat{
+		ID: cat.ID,
+		Name: cat.Name,
+	}
+
+	if err := c.db.Create(&mCat).Error; err != nil {
+		return nil, err
+	}
+
+	return cat, nil
+}
--- adapter/hashid/hash_repository_impl.go
+++ adapter/hashid/hash_repository_impl.go
@@ -1,1 +1,30 @@
 package hashid
+
+import (
+	"errors"
+	"github.com/yuuis/cat-api-go/domain/repository"
+	"github.com/speps/go-hashids"
+
+)
+
+type hash struct {
+	*hashids.HashID
+}
+
+func NewHash(h *hashids.HashID) repository.Hash {
+	return &hash{h}
+}
+
+func (h *hash) Encode(id int64) (string, error) {
+	return h.EncodeInt64([]int64{id})
+}
+
+func (h *hash) Decode(idStr string) (int64, error) {
+	nums, err := h.DecodeInt64WithError(idStr)
+
+	if len(nums) != 1 {
+		return 0, errors.New("decode error")
+	}
+
+	return nums[0], err
+}
