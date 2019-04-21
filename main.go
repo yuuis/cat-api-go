2a44bac88aa0b983da5badf0b12e29040464bb47
Yu Ishiguro <yuuis.egr@gmail.com>
2019-04-21 22:46:51 +0900

    add main file

--- main.go
+++ main.go
@@ -1,1 +1,32 @@
 package cat_api_go
+
+import (
+	"github.com/yuuis/cat-api-go/registry"
+	"github.com/yuuis/cat-api-go/external/api"
+	infrastructure "github.com/yuuis/cat-api-go/inflastructure"
+	"log"
+	"os"
+)
+
+func main()  {
+	hash, err := infrastructure.NewHash()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := infrastructure.OpenDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	logger := log.New(os.Stdout, "cat-api-go", log.Ltime)
+
+	r := registry.NewRegistry(db, hash, logger)
+	controller := r.NewController()
+	server := infrastructure.NewServer()
+	api.NewRouter(server, controller)
+
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
