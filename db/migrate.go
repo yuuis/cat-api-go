4af733da702c9626a16b7f5d46b06ce4aa53d27b
Yu Ishiguro <yuuis.egr@gmail.com>
2019-04-21 22:49:44 +0900

    add migration file

--- db/migrate.go
+++ db/migrate.go
@@ -1,1 +1,21 @@
 package db
+
+import (
+"log"
+
+"github.com/yuuis/cat-api-go/adapter/datastore/mysql"
+
+_ "github.com/go-sql-driver/mysql"
+
+"github.com/jinzhu/gorm"
+)
+
+func main() {
+	db, err := gorm.Open("mysql", "root:password@/diffmvca?charset=utf8mb4&parseTime=true")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.CreateTable(&mysql.Item{})
+}
